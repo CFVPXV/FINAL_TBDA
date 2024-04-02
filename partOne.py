4b7f4f906cc282d4a78ac9f943c51bd05f2f1e5d
CFVPXV <84998466+CFVPXV@users.noreply.github.com>
2024-04-02 00:19:44 +0000

    everything else

--- partOne.py
+++ partOne.py
@@ -1,21 +1,33 @@
+"""
+Program: Assignment 5 Part 1
+
+Author: Nicholas Porter
+
+Description: A K Pyspark program to calculate all of the three-mers in a DNA strand
+"""
 from pyspark.sql import SparkSession
-from pyspark import SparkContext
 import pyspark.sql as spark
 
 spark = SparkSession.builder.appName('cluster').getOrCreate()
 
 sc = spark.sparkContext
 
+# Ingest data
 text_file = sc.textFile("String_data.txt")
 
+# First we will get all possible 3-mers with a list comprehension anonymous function
+# essentially, acting as a sliding window across our data
 out = text_file.flatMap(
     lambda x: [x[i:i+3] for i in range(len(x)-2)]
+    # Then we will map each three-mer found with the anonymous function
     ).map(
         lambda x: (x, 1)
+        # And finally, reduce and sort by the mapped key.
         ).reduceByKey(
             lambda x, y: x + y
             ).sortByKey(True, 1)
 
+# Print out as both a DF and rdd
 out_df = out.toDF()
 
 out_rdd = out.collect()
--- partTwo.py
+++ partTwo.py
@@ -19,25 +19,24 @@ vec_assembler = VectorAssembler(inputCols = ["HeartDisease", "BMI", "Gender", "R
 
 final_df = vec_assembler.transform(df)
 
-# Since BMI is in a different scale from rest of our data,
-#scaler = StandardScaler(inputCol="features",  outputCol="scaledFeatures",  withStd=True,  withMean=False)
-
-#scalerModel = scaler.fit(final_df)
-
-#final_df = scalerModel.transform(final_df)
 # Perform K means with the features column with the proposed 15 K's in the instructions, and a maxIterations of 100
 kmeans=KMeans(featuresCol='features', k=15, maxIter=100, seed=42)
 model=kmeans.fit(final_df)
 predictions=model.transform(final_df)
 
+# Since computeCost is depreceated for getting WSSSE, we are told to use the following Clustering Evaluator
+# by the Spark documentation...
 evaluator = ClusteringEvaluator(predictionCol='prediction', featuresCol='features', metricName='silhouette', distanceMeasure='squaredEuclidean')
 
 wssse = evaluator.evaluate(predictions)
 print(f"Within Set Sum of Squared Errors (WSSSE) = {wssse}")
 
+
+# Find clusters
 centers = model.clusterCenters()
 print("Cluster Centers: ")
 for center in centers:
     print(center)
 
+# And show the predictions
 predictions.select('Pid', 'prediction').show()
